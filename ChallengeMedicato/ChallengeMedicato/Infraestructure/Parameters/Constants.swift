31054ec8724b87842fbabe9b487ee7d7083a1d1f
gaceredo <40033476+gaceredo@users.noreply.github.com>
2021-08-28 19:48:03 -0300

    [add] environment setting (#9)
    Co-authored-by: dvarangotcds <dvarangot@codigodelsur.com>

--- ChallengeMedicato/ChallengeMedicato/Infraestructure/Parameters/Constants.swift
+++ ChallengeMedicato/ChallengeMedicato/Infraestructure/Parameters/Constants.swift
@@ -9,7 +9,12 @@ import Foundation
 
 
 enum Constants {
-    static let apiKey: String = "dfb620b5f8bb704f5cb2b40ea5209237"
+    static var apiKey: String {
+        switch AppEnvironment.current {
+        case .Debug,
+             .Release:  return "dfb620b5f8bb704f5cb2b40ea5209237"
+        }
+    }
 }
 
 enum Keys {
@@ -18,5 +23,10 @@ enum Keys {
 }
 
 enum Path {
-    static let baseImage: String = "https://image.tmdb.org/t/p/w500/"
+    static var baseImage: String {
+        switch AppEnvironment.current {
+        case .Debug,
+             .Release:  return "https://image.tmdb.org/t/p/w500/"
+        }
+    }
 }
